7c3cbbba7facddb8d8659960b7c15d1864a67235
wilwil186 <edujerezwilson@gmail.com>
2024-05-07 09:29:07 -0500

    Añadir informe

--- EstadisticaDescriptiva/Análisis_Descriptivo.R
+++ EstadisticaDescriptiva/Análisis_Descriptivo.R
@@ -1,25 +1,37 @@
 library(ggplot2)
-# Leer los datos
+
 ### Datos ###
 setwd("~/Documentos/Cafe") # Establecer ruta de trabajo
 datosHist <- read.csv('datosHist.csv')
 datosHist$Fecha <- as.Date(datosHist$Fecha)
 ### fin Datos ###
 
-### Serie de timepo ###
+### Series de timepo ###
+## Serie de tiempo de todos los datos ##
 KCN4 <- ts(datosHist$precio_promedio,freq=12)
 plot(datosHist$Fecha, KCN4, type = "l", xlab = "Fecha", 
      ylab = "Precio Promedio", main = "Precios del Café")
+## Serie de tiempo de todos los datos ##
 
-datosHist_2000 <- subset(datosHist, Fecha >= as.Date("2000-01-01"))
-KCN4_2000 <- ts(datosHist_2000$precio_promedio,freq=12)
-plot(datosHist_2000$Fecha, KCN4_2000, type = "l", xlab = "Fecha", 
+# Crear un bucle for para particionar los datos por año
+for (i in 1980:2023) {
+  start_date <- as.Date(paste(i, "-01-01", sep = ""))
+  end_date <- as.Date(paste(i + 1, "-01-01", sep = ""))
+  
+  datosporAño <- subset(datosHist, Fecha >= start_date & Fecha < end_date)
+  KCN4porAño <- ts(datosporAño$precio_promedio, frequency = 12)
+  assign(paste0("Datos_Hist_", i), datosporAño)
+  assign(paste0("KCN4_", i), KCN4porAño)
+}
+
+plot(Datos_Hist_2000$Fecha, KCN4_2000, type = "l", xlab = "Fecha", 
      ylab = "Precio Promedio", main = "Precios del Café")
+
 ### fin Series de tiempo ####
+
 
 ### Cálculo de la variación de las series de tiempo ###
 
-par(mfrow = c(2, 1))
 #plot(diff(KCN4)) # calculo de la variación
 #plot(log(KCN4)) #  útil para estabilizar la varianza y transformar los datos en una escala logarítmica.
 plot(diff(log(KCN4))) 
@@ -34,6 +46,7 @@ abline(h=0)
 ## Descomposición de la serie de tiempo ###
 decomposition <- decompose(KCN4, type = "additive")
 par(mfrow = c(4, 1))
+par(mar = c(3, 3, 1, 1))
 
 plot(decomposition$x, ylab = "Serie de Tiempo Original", xlab = "")
 plot(decomposition$trend, ylab = "Tendencia", xlab = "")
@@ -42,7 +55,8 @@ plot(decomposition$random, ylab = "Residuales", xlab = "")
 
 decomposition_2000 <- decompose(KCN4_2000, type = "additive")
 par(mfrow = c(4, 1))
-
+par(mar = c(3, 3, 1, 1))  
+# Crear las gráficas
 plot(decomposition_2000$x, ylab = "Serie de Tiempo Original", xlab = "")
 plot(decomposition_2000$trend, ylab = "Tendencia", xlab = "")
 plot(decomposition_2000$seasonal, ylab = "Estacionalidad", xlab = "")
@@ -56,22 +70,34 @@ abline(lm(decomposition_2000$x ~ time(decomposition_2000$x)), col = "red")
 
 KCN4_2000 <- log(KCN4_2000) # Estabilización de la varianza
 #PASA TODO A ESCALA LOGARITMICA
-plot(datosHist_2000$Fecha, KCN4_2000, type = "l", xlab = "Fecha", 
+plot(Datos_Hist_2000$Fecha, KCN4_2000, type = "l", xlab = "Fecha", 
      ylab = "Precio Promedio", main = "Precios del Café")
 
 ### Estacionariedad ### 
 
-install.packages("urca")
+#install.packages("urca")
 library(urca)
 
 adf_test <- ur.df(KCN4_2000, type = "trend", lags = 1)
 summary(adf_test)
 
-### AUTOCORRELACIÓN ### 
+### AUTOCORRELACIÓN por año ### 
 
-autocorrelacion<-acf(KCN4_2000, type ="correlation", plot = FALSE)
-autocorrelacion
-plot(autocorrelacion)
+year <- as.integer(readline(prompt = "Ingrese el año a analizar +
+                            (entre 1980 y 2023): "))
+
+# Verificar que el año ingresado esté dentro del rango válido
+if (year < 1980 || year > 2023) {
+  cat("Año inválido. Ingrese un año entre 1980 y 2023.\n")
+} else {
+  # Crear el nombre de la variable de la serie de tiempo
+  kc_name <- paste0("KCN4_", year)
+  
+  # Calcular y graficar la autocorrelación para el año seleccionado
+  autocorrelation <- acf(get(kc_name), type = "correlation", plot = FALSE)
+  plot(autocorrelation)
+}
+
 
 
 
